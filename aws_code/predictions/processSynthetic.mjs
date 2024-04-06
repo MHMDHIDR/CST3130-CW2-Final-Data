36e042af13070a35ea738fcb0ba7d2a64c7ad213
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-06 01:05:01 +0100

    Update file paths in processSynthetic.mjs and index.html

--- aws_code/predictions/processSynthetic.mjs
+++ aws_code/predictions/processSynthetic.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises'
 
 async function process() {
   try {
-    const result = await fs.readFile('./full_data.json', 'utf8')
+    const result = await fs.readFile('./numerical_data_SEK.json', 'utf8')
     const fullData = JSON.parse(result) // Object from file
 
     // Take 100 off end of target
@@ -13,7 +13,7 @@ async function process() {
     console.log(fullData)
 
     // Save to a file
-    await fs.writeFile('train_data.json', JSON.stringify(fullData))
+    await fs.writeFile('train_numerical_data_SEK.json', JSON.stringify(fullData))
     console.log('Processed data saved to train_data.json')
   } catch (error) {
     console.error('Error processing data:', error)
---
+++ aws_code/predictions/query_numerical_data.mjs
@@ -0,0 +1,60 @@
+// Import necessary libraries and commands
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
+import fs from 'fs'
+
+// Create DynamoDB client
+const client = new DynamoDBClient({})
+const docClient = DynamoDBDocumentClient.from(client)
+
+// Function to query DynamoDB table for numerical data with specified currency
+async function queryDynamoDB(currency) {
+  const query = {
+    TableName: 'QRExchangeRates',
+    FilterExpression: 'toCurrency = :curr',
+    ExpressionAttributeValues: {
+      ':curr': currency
+    }
+  }
+
+  try {
+    const queryCommand = new ScanCommand(query)
+    const response = await docClient.send(queryCommand)
+    return response.Items
+  } catch (error) {
+    console.error('Error querying DynamoDB:', error)
+    throw error
+  }
+}
+
+// Function to format data into JSON Lines format
+function formatData(data) {
+  const formattedData = {
+    start: '2014-11-24 00:00:00',
+    target: data.map(item => parseFloat(item.price))
+  }
+  return JSON.stringify(formattedData)
+}
+
+// Function to write data to JSON file
+function writeToJsonFile(data, fileName) {
+  try {
+    fs.writeFileSync(fileName, data)
+    console.log(`Data successfully written to ${fileName}`)
+  } catch (error) {
+    console.error('Error writing to JSON file:', error)
+    throw error
+  }
+}
+
+// Main function to query DynamoDB, format data, and write to JSON file
+async function main() {
+  const currency = 'SEK' // Specify the currency here
+  const data = await queryDynamoDB(currency)
+  const formattedData = formatData(data)
+  const fileName = 'numerical_data_SEK.json'
+  writeToJsonFile(formattedData, fileName)
+}
+
+// Run the main function
+main().catch(console.error)
