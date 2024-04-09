af1617b32cc1a7009dbed91d246f56f7451e5eb6
Mohammed Haydar <mr.hamood277@gmail.com>
2024-04-09 14:54:10 +0100

    Refactor WebSocket message handling, update font styles, and add numerical and sentiment data handling in index.mjs

--- aws_code/wsTableUpdate/index.mjs
+++ aws_code/wsTableUpdate/index.mjs
@@ -16,30 +16,42 @@ const docClient = DynamoDBDocumentClient.from(client);
 export const handler = async event => {
   console.log(JSON.stringify(event));
   try {
-    const data = [];
+    let numericalData = [];
+    let sentimentData = [];
 
     //Extract data from event
     for (let record of event.Records) {
       if (record.eventName === 'INSERT') {
-        //Extract data from event
-        const score = record.dynamodb.NewImage.Score.N;
+        if (record.dynamodb.NewImage.exTimestamp) {
+          // get a hold of that data
+          console.log('record -->', JSON.stringify(record));
 
-        //Extract timestamp
-        const timestamp = record.dynamodb.NewImage.MatchTS.N;
+          const exTimestamp = Number(record.dynamodb.NewImage.exTimestamp.N);
+          const toCurrency = record.dynamodb.NewImage.toCurrency.S;
+          const price = record.dynamodb.NewImage.price.S;
 
-        //Extract team
-        const team = record.dynamodb.NewImage.TeamName.S;
+          numericalData.push({ toCurrency, exTimestamp, price });
+        } else {
+          // get a hold of that data
+          console.log('record -->', JSON.stringify(record));
 
-        data.push({ score, timestamp, team });
+          const TimePublished = Number(record.dynamodb.NewImage.TimePublished.N);
+          const Currency = record.dynamodb.NewImage.Currency.S;
+          const sentiment = record.dynamodb.NewImage.sentiment.N;
+
+          sentimentData.push({ Currency, TimePublished, sentiment });
+        }
       }
     }
 
-    console.log('Message: ', data);
+    console.log('(numericalData) Message: ', numericalData);
+    console.log('(sentimentData) Message: ', sentimentData);
 
     //Get promises to send messages to connected clients
     let sendMsgPromises = await getSendMessagePromises({
-      type: 'numerical',
-      data
+      type: 'numericalAndSentiment',
+      numericalData,
+      sentimentData
     });
 
     //Execute promises
@@ -52,6 +64,16 @@ export const handler = async event => {
   return { statusCode: 200, body: 'Data sent successfully.' };
 };
 
+// Get the IDs for the connected clients
+export async function getConnectionIds() {
+  const scanCommand = new ScanCommand({
+    TableName: 'WebSocketClients'
+  });
+
+  const response = await docClient.send(scanCommand);
+  return response.Items;
+}
+
 //Returns promises to send messages to all connected clients
 export async function getSendMessagePromises(message) {
   //Get connection IDs of clients
@@ -63,8 +85,9 @@ export async function getSendMessagePromises(message) {
 
   //Try to send message to connected clients
   let msgPromiseArray = clientIdArray.map(async item => {
-    //Extract connection ID
+    // Get connected IDs
     const connId = item.ConnectionId;
+
     try {
       //Create post to connection command
       const postToConnectionCommand = new PostToConnectionCommand({
@@ -76,36 +99,13 @@ export async function getSendMessagePromises(message) {
       await apiGwClient.send(postToConnectionCommand);
     } catch (err) {
       console.log('Failed to send message to: ' + connId);
-
-      //Delete connection ID from database
-      if (err.statusCode == 410) {
-        try {
-          await deleteConnectionId(connId);
-        } catch (err) {
-          console.log('ERROR deleting connectionId: ' + JSON.stringify(err));
-          throw err;
-        }
-      } else {
-        console.log('UNKNOWN ERROR: ' + JSON.stringify(err));
-        throw err;
-      }
     }
   });
 
   return msgPromiseArray;
 }
 
-//Returns all of the connection IDs
-export async function getConnectionIds() {
-  const scanCommand = new ScanCommand({
-    TableName: 'WebSocket'
-  });
-
-  const response = await docClient.send(scanCommand);
-  return response.Items;
-}
-
-//Deletes the specified connection ID
+// Deleting the specified connection ID
 export async function deleteConnectionId(connectionId) {
   console.log('Deleting connection Id: ' + connectionId);
 
